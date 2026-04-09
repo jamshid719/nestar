e14fee0cea59893ed946f232da4ee18031d33bf9
FLOYD <j.shaymanov786@gmail.com>
2026-04-09 15:22:09 +0900

    feat: creation and verification jwt

--- apps/nestar-api/src/components/auth/auth.service.ts
+++ apps/nestar-api/src/components/auth/auth.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import * as bcrypt from 'bcryptjs';
+import { JwtService } from '@nestjs/jwt';
+import { Member } from '../../libs/dto/member/member';
+import { T } from '../../libs/types/common';
 
 @Injectable()
 export class AuthService {
+	constructor(private jwtService: JwtService) {} //shu orqali tokenni hosil qilamiz
+
 	public async hashPassword(memberPassword: string): Promise<string> {
 		const salt = await bcrypt.genSalt();
 		return await bcrypt.hash(memberPassword, salt);
@@ -10,5 +15,21 @@ export class AuthService {
 
 	public async comparePasswords(password: string, hashedPassword: string): Promise<boolean> {
 		return await bcrypt.compare(password, hashedPassword);
+	}
+
+	public async createToken(member: Member): Promise<string> {
+		const payload: T = {}; //payload orqali tokenni hosil qilamiz.
+
+		Object.keys(member['_doc'] ? member['_doc'] : member).map((ele) => {
+			payload[`${ele}`] = member[`${ele}`];
+		}); // bunday qilishimizdan maqsad, member har doim uzi bn kelmasligi mumkin, yana '_doc' ichida ham kelishi mumkin.
+		delete payload.memberPassword; // payload da memberPasswordni uchiriwimiz kk.
+		console.log('payload:', payload);
+		return await this.jwtService.signAsync(payload);
+	}
+
+	public async verifyToken(token: string): Promise<Member> {
+		const member = await this.jwtService.verifyAsync(token);
+		return member;
 	}
 }
--- apps/nestar-api/src/components/member/member.service.ts
+++ apps/nestar-api/src/components/member/member.service.ts
@@ -20,7 +20,9 @@ export class MemberService {
 
 		try {
 			const result = await this.memberModel.create(input);
-			//TODO: Authentication via TOKEN
+			//Authentication via TOKEN
+			result.accessToken = await this.authService.createToken(result);
+
 			return result;
 		} catch (err) {
 			console.log('Error, Service.model:', err.message);
@@ -42,9 +44,10 @@ export class MemberService {
 		}
 
 		//compare passwords
-
 		const isMatch = await this.authService.comparePasswords(input.memberPassword, response.memberPassword);
 		if (!isMatch) throw new InternalServerErrorException(Message.WRONG_PASSWORD);
+
+		response.accessToken = await this.authService.createToken(response);
 		return response;
 	}
 
