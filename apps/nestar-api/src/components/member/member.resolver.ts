843f58a9f9767c66d224ede22d07b6f15f9f4e87
FLOYD <j.shaymanov786@gmail.com>
2026-04-14 00:51:27 +0900

    feat: develop getAgents GraphQL api

--- apps/nestar-api/src/components/member/member.resolver.ts
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -1,8 +1,8 @@
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { MemberService } from './member.service';
 import { InternalServerErrorException, UseGuards, UsePipes, ValidationPipe } from '@nestjs/common';
-import { LoginInput, MemberInput } from '../../libs/dto/member/member.input';
-import { Member } from '../../libs/dto/member/member';
+import { AgentsInquiry, LoginInput, MemberInput } from '../../libs/dto/member/member.input';
+import { Member, Members } from '../../libs/dto/member/member';
 import { AuthGuard } from '../auth/guards/auth.guard';
 import { AuthMember } from '../auth/decorators/authMember.decorator';
 import { ObjectId } from 'mongoose';
@@ -62,7 +62,7 @@ export class MemberResolver {
 		return `Hi, ${authMember.memberNick}, you are ${authMember.memberType} (memberId: ${authMember._id})`;
 	}
 
-	@UseGuards(WithoutGuard) //Auth member bulsa bulmasa kngi bosqichga utkaz degani.
+	@UseGuards(WithoutGuard) //Auth member(ixtiyoriy member) bulsa bulmasa kngi bosqichga utkaz degani.
 	@Query(() => Member)
 	public async getMember(@Args('memberId') input: string, @AuthMember('_id') memberId: ObjectId): Promise<Member> {
 		console.log('Query: getMember');
@@ -70,8 +70,14 @@ export class MemberResolver {
 		return this.memberService.getMember(memberId, targetId);
 	}
 
-	/**ADMIN */
+	@UseGuards(WithoutGuard) //ixtiyoriy member ishlata oladibu apini
+	@Query(() => Members)
+	public async getAgents(@Args('input') input: AgentsInquiry, @AuthMember('_id') memberId: ObjectId): Promise<Members> {
+		console.log('Query: getAgents');
+		return this.memberService.getAgents(memberId, input);
+	}
 
+	/**ADMIN */
 	@Roles(MemberType.ADMIN)
 	@UseGuards(RolesGuard)
 	@Mutation(() => String)
--- apps/nestar-api/src/libs/config.ts
+++ apps/nestar-api/src/libs/config.ts
@@ -1,5 +1,7 @@
 import { ObjectId } from 'bson';
 
+export const availableAgentSorts = ['createdAt', 'updatedAt', 'memberLikes', 'memberViews', 'memberRank']; //memberAgentlarni shular buyicha sort qilish mexanizmi
+
 export const shapeIntoMongooseObjectId = (target: any) => {
 	return typeof target === 'string' ? new ObjectId(target) : target;
 };
