3e45ee94d6b40f988f0f5cfade03f03365bc9f95
FLOYD <j.shaymanov786@gmail.com>
2026-04-18 18:23:37 +0900

    feat: develop updatePropertyByAdmin business logic

--- apps/nestar-api/src/components/property/property.resolver.ts
+++ apps/nestar-api/src/components/property/property.resolver.ts
@@ -91,4 +91,14 @@ export class PropertyResolver {
 
 		return await this.propertyService.getAllPropertiesByAdmin(input);
 	}
+
+	@Roles(MemberType.ADMIN)
+	@UseGuards(RolesGuard)
+	@Mutation((returns) => Property)
+	public async updatePropertyByAdmin(@Args('input') input: PropertyUpdate): Promise<Property> {
+		console.log('Mutation: updatePropertyByAdmin');
+		input._id = shapeIntoMongooseObjectId(input._id);
+
+		return await this.propertyService.updatePropertyByAdmin(input);
+	}
 }
