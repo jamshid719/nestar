be85fd49ea5e96e34e409dd1a139fc7c9fa93f46
FLOYD <j.shaymanov786@gmail.com>
2026-04-21 01:03:20 +0900

    feat: develop removeCommentByAdmin Graphql api

--- apps/nestar-api/src/components/comment/comment.service.ts
+++ apps/nestar-api/src/components/comment/comment.service.ts
@@ -107,4 +107,10 @@ export class CommentService {
 
 		return result[0];
 	}
+
+	public async removeCommentByAdmin(commentId: ObjectId): Promise<Comment> {
+		const result = await this.commentModel.findByIdAndDelete(commentId).exec(); //DB dan butunlay uchirish
+		if (!result) throw new InternalServerErrorException(Message.REMOVE_FAILED);
+		return result;
+	}
 }
