d8c321bc07ccf1bcc3d00066e4faea6a3d19ba60
FLOYD <j.shaymanov786@gmail.com>
2026-04-22 14:50:07 +0900

    feat: create likeService model

--- apps/nestar-api/src/components/like/like.module.ts
+++ apps/nestar-api/src/components/like/like.module.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common';
+import LikeSchema from '../../schemas/Like.model';
+import { MongooseModule } from '@nestjs/mongoose';
+import { LikeService } from './like.service';
 
-@Module({})
+@Module({
+	imports: [MongooseModule.forFeature([{ name: 'Like', schema: LikeSchema }])],
+	providers: [LikeService],
+	//Bu modelning xususiyati shundan iboratki, faqat likeService modeli buladi, resolveri bulmaydi, sababi Like modulega tegishli bulgan tugridan tugri graphQL apilar kk bulmaydi(uni urniga , agar memberlarga oid like buladigan bulsa, uni memberResolverning ichida graphql apilarni hosil qilamiz.). Bular bowqa modullarning mantigi un xizmatga keladi.
+	exports: [LikeService],
+})
 export class LikeModule {}
