5b578db4f49224d2c91b16c447b2a28c35437019
FLOYD <j.shaymanov786@gmail.com>
2026-05-26 13:56:03 +0900

    feat: develop Websocket Chatting Server first stage

--- apps/nestar-api/src/socket/socket.gateway.ts
+++ apps/nestar-api/src/socket/socket.gateway.ts
@@ -1,6 +1,19 @@
 import { Logger } from '@nestjs/common';
-import { OnGatewayInit, SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import { OnGatewayInit, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { Server } from 'ws';
+import * as WebSocket from 'ws';
+
+//FR.ga datalarni yuborish va qabul qilish interface
+interface MessagePayload {
+	event: string;
+	text: string;
+}
+
+//Yangi user qushilganda qolgan userlarga data yuborish kk buladi
+interface InfoPayload {
+	event: string;
+	totalClients: number;
+}
 
 //@WebSocketGateway - bu class WebSocket server ekanini bildiradi
 @WebSocketGateway({ transports: ['websocket'], secure: false })
@@ -8,25 +21,62 @@ export class SocketGateway implements OnGatewayInit {
 	private logger: Logger = new Logger('SocketEventsGateway');
 	private summaryClient: number = 0; // nechta user ulanganini sanaydi.
 
+	@WebSocketServer()
+	server: Server;
+
 	public afterInit(server: Server) {
-		this.logger.log(`WebSocket Server Initialized total: ${this.summaryClient}`); //WebSocket server ishga tushganda bir marta ishlaydi va server start bulganda log chiqaradi
+		this.logger.verbose(`WebSocket Server Initialized & total [${this.summaryClient}]`); //WebSocket server ishga tushganda bir marta ishlaydi va server start bulganda log chiqaradi
 	}
 
 	// yangi user connect/disconnect bulganda ishlaydigan mantiqlar
 	handleConnection(client: WebSocket, ...args: any[]) {
 		this.summaryClient++; // bir user ulanganda sonini 1taga oshiriw
-		this.logger.log(`== Client connected total: ${this.summaryClient} ==`);
+		this.logger.verbose(`Connection & total [${this.summaryClient}]`);
+
+		//Yangi user ulanganligini, boshqalarga malum qilish
+		const infoMsg: InfoPayload = {
+			event: 'info',
+			totalClients: this.summaryClient, //tepeda 1taga oshgan
+		};
+		this.emitMessage(infoMsg);
 	}
 
 	handleDisconnect(client: WebSocket) {
 		this.summaryClient--; // bir user disconnect bulganda sonini 1taga kamaytirish
-		this.logger.log(`== Client disconnected left total: ${this.summaryClient} ==`);
+		this.logger.verbose(`Disconnection & total [${this.summaryClient}]`);
+
+		const infoMsg: InfoPayload = {
+			event: 'info',
+			totalClients: this.summaryClient,
+		};
+		//client - disconnect
+		this.broadcastMessage(client, infoMsg);
 	}
 
 	@SubscribeMessage('message')
-	public handleMessage(client: WebSocket, payload: any): string {
-		return 'Hello world!';
+	public async handleMessage(client: WebSocket, payload: string): Promise<void> {
+		const newMessage: MessagePayload = { event: 'message', text: payload };
+		this.logger.verbose(`NEW MESSAGE: ${payload}`);
+		this.emitMessage(newMessage);
 	} //Client {"event": "message"} yuborsa, server javob beradi "Hello world!" deb.
+
+	//Disconnect bulgan userdan tashqari bowqal hammaga xabar yuborish
+	private broadcastMessage(sender: WebSocket, message: InfoPayload | MessagePayload) {
+		this.server.clients.forEach((client) => {
+			if (client !== sender && client.readyState === WebSocket.OPEN) {
+				client.send(JSON.stringify(message));
+			}
+		});
+	}
+
+	//Yangi user kirganligini qolganlarga xabar berish methodi
+	private emitMessage(message: InfoPayload | MessagePayload) {
+		this.server.clients.forEach((client) => {
+			if (client.readyState === WebSocket.OPEN) {
+				client.send(JSON.stringify(message));
+			}
+		});
+	}
 }
 
 /**
